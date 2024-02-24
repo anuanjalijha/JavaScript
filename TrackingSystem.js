ac9701421c2081e99b2fd7ef6ad79e363c92fd75
Anjali <anjalijanar@gmail.com>
2024-02-24 12:47:52 +0530

    You have been hired to develop a tracking system for a logistics company. The system should allow the company to keep track of the location and status of each shipment, as well as assign resources to each shipment as needed.  Create a constructor function called Shipment with properties such as
    *Note: All the properties should be assigned with the values at the time of the object creation id: a string representing the shipment ID
    
    location: string representing the location of shipment
    
    destination: string representing destination for the shipment
    
    status : string representing the status of the shipment
    
    resources : an array containing all the resources that have been assigned to the shipment
    
    Using the prototype of the Shipment constructor function, add the following methods
    updateStatusandResources :
    should take status and resources as arguments and update the status and resource property of the object.
    
    assignResources : This method should be able to push the arguments passed to the resource array.
    
    (Note: the number of arguments passed by the user is not defined. It can be any number of arguments)
    Create an object called TrackingSystem using an object literal that has the following:
    shipments(property): representing an array of shipment object
    
    updateStatus(method) : It should take two parameters, id and status, and should be able to change the status of the object using the id of the object.
    (Hint: You can use various array methods to iterate over the array)
    
    viewShipment(method) : It should take id as a parameter and should display the details of that shipment like id, status, resources, location, and destination.
    ** use the concept of destructuring in viewShipment() method to display all the properties.

---
+++ TrackingSystem.js
@@ -0,0 +1,76 @@
+// function main(){
+    //Create your constructor funtion with the name Shipment
+    
+    //Add the updateStatusAndResources using the object prototype
+    
+    //Add the assignResource methods through Object prototype
+    
+    //Create the object literal with tthe name TrackingSystem
+    
+    // return {Shipment,TrackingSystem};
+    //do not modify any other code.
+    // }
+    //solution
+    function main() {
+        // Create constructor function Shipment
+        function Shipment(id, location, destination, status, resources) {
+          this.id = id;
+          this.location = location;
+          this.destination = destination;
+          this.status = status;
+          this.resources = resources;
+        }
+      
+        // Add updateStatusAndResources using Object prototype
+        Shipment.prototype.updateStatusAndResources = function (status, resources) {
+          this.status = status;
+          this.resources = resources;
+        };
+      
+        // Add assignResources using Object prototype
+        Shipment.prototype.assignResources = function (...args) {
+          this.resources.push(...args);
+        };
+      
+        // Create object literal TrackingSystem
+        const TrackingSystem = {
+          shipments: [],
+      
+          updateStatus: function (id, status) {
+            const shipment = this.shipments.find(shipment => shipment.id === id);
+            if (shipment) {
+              shipment.status = status;
+            }
+          },
+      
+          viewShipment: function (id) {
+            const shipment = this.shipments.find(shipment => shipment.id === id);
+            if (shipment) {
+              const { id, status, resources, location, destination } = shipment;
+              console.log(`Shipment ID: ${id}`);
+              console.log(`Status: ${status}`);
+              console.log(`Resources: ${resources.join(', ')}`);
+              console.log(`Location: ${location}`);
+              console.log(`Destination: ${destination}`);
+            }
+          }
+        };
+      
+        return { Shipment, TrackingSystem };
+      }
+      
+      // Example usage
+      const { Shipment, TrackingSystem } = main();
+      
+      const shipment1 = new Shipment("12345", "New York", "Los Angeles", "En route", ["Driver", "Truck"]);
+      shipment1.updateStatusAndResources("Delayed", ["Forklift"]);
+      shipment1.assignResources("Worker", "Pallets");
+      
+      const shipment2 = new Shipment("67890", "Chicago", "Miami", "In transit", ["Forklift"]);
+      shipment2.updateStatusAndResources("In transit", ["Driver"]);
+      
+      TrackingSystem.shipments.push(shipment1, shipment2);
+      
+      TrackingSystem.updateStatus("12345", "Delivered");
+      TrackingSystem.viewShipment("12345");
+      
