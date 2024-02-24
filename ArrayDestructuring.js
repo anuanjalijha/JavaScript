10a3a7f74e8f78642e76e32fc9250994167ff7b3
Anjali <anjalijanar@gmail.com>
2024-02-24 12:45:00 +0530

    array destructuring example

---
+++ ArrayDestructuring.js
@@ -0,0 +1,17 @@
+const fruits = [
+    'Apple',
+    'Mango',
+    'Kiwi',
+    'Berry',
+    'banana',
+    'lichi',
+  ];
+  
+  //Binding Pattern
+  
+  // const [a, , , k] = fruits;
+  // console.log(a, k);
+  
+  const [a, m, ...[, be, ba]] = fruits;
+  console.log(ba);
+  
