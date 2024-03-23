9039a9bc0b502afefdc772d38026ae2fe78f1b9b
Anjali <anjalijanar@gmail.com>
2024-03-23 11:02:27 +0530

    js file for name import and import html file

---
+++ mod1.js
@@ -0,0 +1,10 @@
+// import {obj as obj1, add } from './mod2';
+// const filePath = './mod2.js';
+import { obj } from './mod2.js';
+
+// import * as something from './mod2.js';
+
+// console.log (something);
+// console.log(something. obj);
+// console.log(something.add(2, 3));
+console.log(obj);
